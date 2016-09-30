134e9d50af181b2c6f3e7bd3766da34e79f03a85
james-lawlor <jlawlor@hudsonalpha.org>
2016-09-30 13:57:37 -0500

    Basic functionality complete

--- check_bam_eof.py
+++ check_bam_eof.py
@@ -13,9 +13,9 @@
 
 import argparse
 import sys
+import subprocess
 
-
-
+true_eof = ['1f', '8b', '08', '04', '00', '00', '00', '00', '00', 'ff', '06', '00', '42', '43', '02', '00', '1b', '00', '03', '00', '00', '00', '00', '00', '00', '00', '00', '00']
 parser = argparse.ArgumentParser()
 parser.add_argument("bams", nargs='*', help="One or more filenames/filepaths of BAMs to check for EOF")
 parser.add_argument("-f", "--bam_list_filename", help="A list of filenames/filepaths of BAMs to check for EOF")
@@ -36,5 +36,30 @@ if (args.bams != []):
 else:
     with open(args.bam_list_filename,"r") as f:
         bams = f.read().splitlines()
+
+
+for bam_file in bams:
+    cmd = "tail -n 1 " + bam_file + " | hexdump -C | tail -n 5"
+    command = subprocess.Popen(cmd, shell=True, executable='/bin/bash', stdout=subprocess.PIPE)
+    (bam_tail, err) = command.communicate()
+    bam_tail_words = bam_tail.rsplit(' ')
+    #transform bam_tail from string into list of strings
+
+    tail_bits = []
+
+    #canonical hexdump returns 2-column hex bytes, offset position, and ACSII strings. Only the hex bytes will have len of 2
+    for item in bam_tail_words:
+        if len(item) != 2:
+            continue
+        else:
+            tail_bits.append(item)
+
+    candidate_eof = tail_bits[-28:]
+    #candidate_eof is now the last 28 bytes of the file, in order
+
+    if candidate_eof == true_eof:
+        continue
+    else:
+        print "Could not find EOF for: " + bam_file
 
 
